110167b494440347cbfe2da4444707cef34fb3d1
ryanp4096 <ryanp4096@gmail.com>
2026-09-16 01:26:10 -0700

    subpath length limit config option

--- batch/config.py
+++ batch/config.py
@@ -17,6 +17,7 @@ class Config:
             finish_lkh_before_bb: bool = None,
             process_lkh_subpaths: bool = None,
             subpath_history_table: bool = None,
+            subpath_length_limit: int = None,
             trace: bool = None,
             trace_detail_level: int = None,
             background: bool = None,
@@ -33,6 +34,7 @@ class Config:
         self.finish_lkh_before_bb = finish_lkh_before_bb
         self.process_lkh_subpaths = process_lkh_subpaths
         self.subpath_history_table = subpath_history_table
+        self.subpath_length_limit = subpath_length_limit
         self.trace = trace
         self.trace_detail_level = trace_detail_level
         self.background = background
@@ -55,6 +57,7 @@ class Config:
                 finish_lkh_before_bb = config.finish_lkh_before_bb,
                 process_lkh_subpaths = config.process_lkh_subpaths,
                 subpath_history_table = config.subpath_history_table,
+                subpath_length_limit = config.subpath_length_limit,
                 trace = config.trace,
                 trace_detail_level = config.trace_detail_level,
                 background = config.background,
@@ -75,6 +78,7 @@ class Config:
             finish_lkh_before_bb: bool = None,
             process_lkh_subpaths: bool = None,
             subpath_history_table: bool = None,
+            subpath_length_limit: int = None,
             trace: bool = None,
             trace_detail_level: int = None,
             background: bool = None,
@@ -91,6 +95,7 @@ class Config:
         if finish_lkh_before_bb is not None: self.finish_lkh_before_bb = finish_lkh_before_bb
         if process_lkh_subpaths is not None: self.process_lkh_subpaths = process_lkh_subpaths
         if subpath_history_table is not None: self.subpath_history_table = subpath_history_table
+        if subpath_length_limit is not None: self.subpath_length_limit = subpath_length_limit
         if trace is not None: self.trace = trace
         if trace_detail_level is not None: self.trace_detail_level = trace_detail_level
         if background is not None: self.background = background
@@ -167,6 +172,9 @@ TRACE_DETAIL_LEVEL = {self.trace_detail_level}
 
 // Enable separate history table for subpaths (1 for enable, 0 for disable)
 ENABLE_SUBPATH_HISTORY_TABLE = {1 if self.subpath_history_table else 0}
+
+// The maximum length of subpaths included in subpath history table (0 = no limit)
+SUBPATH_LENGTH_LIMIT = {self.subpath_length_limit}
 '''
     
     def dump(self):
@@ -181,6 +189,7 @@ ENABLE_SUBPATH_HISTORY_TABLE = {1 if self.subpath_history_table else 0}
             'finish_lkh_before_bb': self.finish_lkh_before_bb,
             'process_lkh_subpaths': self.process_lkh_subpaths,
             'subpath_history_table': self.subpath_history_table,
+            'subpath_length_limit': self.subpath_length_limit,
             'trace': self.trace,
             'trace_detail_level': self.trace_detail_level,
             'background': self.background,
@@ -204,6 +213,7 @@ DEFAULT_CONFIG = Config(
     finish_lkh_before_bb = False,
     process_lkh_subpaths = True,
     subpath_history_table = False,
+    subpath_length_limit = 0,
     trace = False,
     trace_detail_level = 0,
     background = False,
